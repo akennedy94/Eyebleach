48534b45fa5a2d83f91866616a3b855ee9ec077f
akennedy94 <71675206+akennedy94@users.noreply.github.com>
2021-01-11 11:43:15 -0600

    Updates to CSS, build, and readme

--- src/server.js
+++ src/server.js
@@ -6,8 +6,7 @@ const path = require("path");
 
 app.use(cors())
     .use(express.json())
-    .use("/public", express.static(path.join(__dirname, "../public")))
-    .use(express.static("../build"));
+    .use('/', express.static("../build"));
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
